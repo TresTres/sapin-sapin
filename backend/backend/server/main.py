ff31a3d1d2163ccf14b4e4badd2ce0f691b3c684
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-01-27 00:59:19 -0500

    replace fastapi with flask

--- backend/backend/server/main.py
+++ backend/backend/server/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from flask import Flask
+from backend.models import * 
 
 
-app = FastAPI()
+app = Flask(__name__)
+
+
+
+@app.before_request
+def _db_connect() -> None:
+    sql_db.connect()
+    
+@app.teardown_request
+def _db_close(exc: Exception) -> None:
+    if not sql_db.is_closed():
+        sql_db.close()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/user")
+def create_user():
+    pass 
