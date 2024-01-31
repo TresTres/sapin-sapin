c5b551b5a5e7a925cef1e490bb36a361a2b388f8
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-01-30 23:42:46 -0500

    use state to alter rendered routes

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,55 +1,48 @@
 <template>
   <header>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/grids-responsive-min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/grids-responsive-min.css"
+    />
   </header>
   <NuxtLoadingIndicator />
   <NuxtLayout>
-    <Navbar :routes="[
-      {
-        name: 'Home',
-        path: ''
-      },
-      {
-        name: 'Register',
-        path: 'registration'
-      },
-      {
-        name: 'Login',
-        path: 'login'
-      }
-    ]"/>
+    <Navbar
+      :routes="[...appRoutes, ...(userStore.loginStatus ? userRoutes : landingRoutes)]"
+    />
     <NuxtPage />
   </NuxtLayout>
-  <footer>
-
-  </footer>
+  <footer></footer>
 </template>
 
 <script setup lang="ts">
-import 'purecss'
+import "purecss";
+
+const userStore = getUserStore();
+
 </script>
 
 <style scoped>
-  header {
-    background-color: #671ec1;
-  }
-  nav {
-    background-color: #671ec1;
-  }
-  nav ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  nav ul li {
-    display: inline;
-    margin-right: 10px;
-  }
-  nav ul li a {
-    color: white;
-    text-decoration: none;
-  }
-  footer {
-    background-color: #671ec1;
-  }
+header {
+  background-color: #671ec1;
+}
+nav {
+  background-color: #671ec1;
+}
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+nav ul li {
+  display: inline;
+  margin-right: 10px;
+}
+nav ul li a {
+  color: white;
+  text-decoration: none;
+}
+footer {
+  background-color: #671ec1;
+}
 </style>
---
+++ frontend/composables/routes.ts
@@ -0,0 +1,26 @@
+export const appRoutes = [
+  {
+    name: "Home",
+    path: "",
+  },
+];
+export const landingRoutes = [
+  {
+    name: "Register",
+    path: "registration",
+  },
+  {
+    name: "Login",
+    path: "login",
+  },
+];
+export const userRoutes = [
+  {
+    name: "Profile",
+    path: "profile",
+  },
+  {
+    name: "Logout",
+    path: "logout",
+  },
+];
