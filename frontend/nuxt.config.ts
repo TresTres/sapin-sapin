620fed9068553542553ebedff6b028c602d23caa
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-01-30 23:37:36 -0500

    create user store

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,11 +2,24 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-
   runtimeConfig: {
     public: {
-      backendUrl: process.env.BASE_URL || 'http://127.0.0.1:8000',
-      apiVersion: process.env.API_VERSION || 'DEV',
-    }
+      backendUrl: process.env.BASE_URL || "http://127.0.0.1:8000",
+      apiVersion: process.env.API_VERSION || "DEV",
+    },
+  },
+
+  modules: [
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore"],
+      },
+    ],
+  ],
+  imports: {
+    dirs: [
+      "stores",
+    ]
   }
-})
+});
