5e4bcfed18485dd4050cda05b93b4221b95e1969
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-02-06 02:20:09 -0500

    login frontend form

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,25 +1,22 @@
+import { UserResponseObject, UserObject } from '../stateless/interfaces/response-objects';
 <template>
   <form class="pure-form-1-2 pure-form-aligned" @submit.prevent="handleLogin">
     <fieldset class="pure-group">
-      <span>{{ userLoginError }}</span>
-      <div class="pure-control-group">
-        <label for="aligned-identifier">Username or Email</label>
-        <input
-            id="aligned-identifier"
-            type="text"
-            class="pure-input-1-2"
-            placeholder="Username or Email"
-            v-model="identifier"
+      <span v-text="userLoginError"></span>
+      <div>
+        <UserFormInput
+          :label="identifierLabel"
+          :index=0
+          :isPassword=false
+          :placeholder="identifierLabel"
+          v-model:inputValue="identifier"
         />
-      </div>
-      <div class="pure-control-group">
-        <label for="aligned-password">Password</label>
-        <input
-            id="aligned-password"
-            type="password"
-            class="pure-input-1-2"
-            placeholder="Password"
-            v-model="password"
+        <UserFormInput
+          :label="passwordLabel"
+          :index=1
+          :isPassword=true
+          :placeholder="passwordLabel"
+          v-model:inputValue="password"
         />
       </div>
     </fieldset>
@@ -27,59 +24,54 @@
       type="submit"
       class="pure-button pure-input-1-2 pure-button-primary"
     >
-      Sign in
+      Login
     </button>
   </form>
 </template>
-
 
 <script setup lang="ts">
 definePageMeta({
   layout: "landing",
 });
 
-const identifier = defineModel('identifier', {
+const userLoginError = defineModel("userLoginError", {
   type: String,
   default: "",
 });
-const password = defineModel('password', {
-  type: String,
-  default: "",
-});
-const userLoginError = defineModel('userLoginError', {
-      type: String,
-      default: "",
-    });
+const identifierLabel = "Username or Email";
+const passwordLabel = "Password";
+const identifier = ref("");
+const password = ref("");
+
+
+const userStore = getUserStore();
+
 
 const handleLogin = async (): Promise<void> => {
-
-    const userStore = getUserStore();
-    await useBaseFetch("/login", {
-      method: "POST",
-      body: JSON.stringify({
-        identifier: identifier.value,
-        password: password.value,
-      }),
-    }).then(({data, error}) => {
-        if (error) {
-          const errorContent = error.value
-          if(errorContent?.statusCode === 401) {
-            userLoginError.value = "Invalid username or password"
-          } else {
-            userLoginError.value = "An error occurred"
-            throw error;
-          }
-          return;
+  await useBaseFetch("/login", {
+    method: "POST",
+    body: JSON.stringify({
+      identifier: identifier.value,
+      password: password.value,
+    }),
+  })
+    .then(({ data, error }) => {
+      if (error) {
+        const errorContent = error.value;
+        if (errorContent?.statusCode === 401) {
+          userLoginError.value = "Invalid username or password";
+        } else {
+          userLoginError.value = "An error occurred";
+          throw error;
         }
-        // const { user } = data.value;
-        // userStore.login(user.username)
+        return;
+      }
+      const user = data?.value;
+      userStore.login((user as UserObject).username);
     })
     .catch((error) => {
       console.log(error);
       //throw error;
     });
-
 };
-
-
 </script>
