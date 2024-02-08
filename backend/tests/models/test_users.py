6edcac6cc3a5303cf0ba50f9b2d3072629323038
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-02-08 01:11:19 -0500

    extract out constants for tests

--- backend/tests/models/test_users.py
+++ backend/tests/models/test_users.py
@@ -4,14 +4,22 @@ import pytest
 
 from backend.models.users import User
 from backend.models.base import ValidationError
-
-VALID_USERNAME="test_user"
-VALID_EMAIL="test_email@domain.com"
-VALID_PASSWORD="Haoot_Birthy_42!"
+from tests.constants import *
 
 
 class TestUserModel:
     
+    
+    def test_create_user_failure_no_date_joined(self) -> None:
+        """
+        Test exception when creating a user without a date_joined
+        """
+        with pytest.raises(peewee.IntegrityError):
+            User.create(
+                username=VALID_USERNAME,
+                email=VALID_EMAIL,
+                password=VALID_PASSWORD,
+            )
     @pytest.mark.parametrize(
         "username",
         [
@@ -20,7 +28,7 @@ class TestUserModel:
             ("a" * 36),  
         ],
     )
-    def test_create_user_failure_invalid_username_length(self, username: str, db: peewee.SqliteDatabase) -> None:
+    def test_create_user_failure_invalid_username_length(self, username: str) -> None:
         """
         Test exception when creating a user with an invalid username:
         - username is empty
@@ -32,7 +40,7 @@ class TestUserModel:
                 username=username,
                 email=VALID_EMAIL,
                 password=VALID_PASSWORD,
-                date_joined=datetime.datetime(2020, 1, 1),
+                date_joined=VALID_JOIN_DATE,
             )
 
     @pytest.mark.parametrize(
@@ -52,7 +60,7 @@ class TestUserModel:
             ("test_email@domain.f"),
         ],
     )
-    def test_create_user_failure_invalid_email(self, email: str, db: peewee.SqliteDatabase) -> None:
+    def test_create_user_failure_invalid_email(self, email: str) -> None:
         """
         Test exception when creating a user with an invalid email:
         - email is empty
@@ -69,7 +77,7 @@ class TestUserModel:
                 username=VALID_USERNAME,
                 email=email,
                 ppassword=VALID_PASSWORD,
-                date_joined=datetime.datetime(2020, 1, 1),
+                date_joined=VALID_JOIN_DATE,
             )
     
     @pytest.mark.parametrize(
@@ -84,13 +92,15 @@ class TestUserModel:
             ("2ASDFASDF!"),  
             ("asdas!43442"),  
             ("test_password"),  
-            ("Test_Password1"),  
+            ("Test-Password1"), 
+            ("Test;Password2!"), 
+            ("Test Password2!"),
             ("testpassword!"),  
             ("TESTPASSWORD!"),  
             ("TestPassword123"),  
         ]
     )
-    def test_create_user_failure_invalid_password_length(self, password, db: peewee.SqliteDatabase) -> None:
+    def test_create_user_failure_invalid_password_length(self, password) -> None:
         """
         Test exception when creating a user without a password:
         - password is too short
@@ -99,26 +109,27 @@ class TestUserModel:
         - password does not contain a lowercase letter
         - password does not contain an uppercase letter
         - password does not contain a special character
+        - password contains a disallowed special character
         """
         with pytest.raises(ValidationError):
             User.create(
                 username=VALID_USERNAME,
                 email=VALID_EMAIL,
                 password=password,
-                date_joined=datetime.datetime(2020, 1, 1),
+                date_joined=VALID_JOIN_DATE,
             )
         
             
-    def test_create_user_success(self, db: peewee.SqliteDatabase) -> None:
+    def test_create_user_success(self) -> None:
         """
         Test successful user creation.
         """
 
-        user = User.create(
+        User.create(
             username=VALID_USERNAME,
             email=VALID_EMAIL,
             password=VALID_PASSWORD,
-            date_joined=datetime.datetime(2020, 1, 1),
+            date_joined=VALID_JOIN_DATE,
         )
         
         assert User.select().count() == 1
@@ -126,5 +137,5 @@ class TestUserModel:
         assert db_user.username == VALID_USERNAME
         assert db_user.email == VALID_EMAIL
         assert db_user.password.startswith("$2b$12$")
-        assert db_user.date_joined == datetime.datetime(2020, 1, 1)
+        assert db_user.date_joined == VALID_JOIN_DATE
         assert db_user.is_active == True
