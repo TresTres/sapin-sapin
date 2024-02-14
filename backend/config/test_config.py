4d9721424860f0ab23e9967070a72ad7555b4485
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-02-14 01:08:21 -0500

    adjust key secrets in config

--- backend/config/test_config.py
+++ backend/config/test_config.py
@@ -1,6 +1,6 @@
 DEBUG = True
 TESTING = True
-DATABASE_URL = "./sqlite/test.db"
+DATABASE_URL = "./sqlite/test_core.db"
 ACCESS_KEY_SECRET = "testsecretkey33"
 REFRESH_KEY_SECRET = "testrefreshkey43"
 API_VERSION = "TEST"
