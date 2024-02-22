1b393a71caa6fb863499942c6984c3adcedc9e8f
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-02-22 03:18:51 -0500

    implement landing card layout

--- frontend/layouts/landing.vue
+++ frontend/layouts/landing.vue
@@ -1,39 +1,46 @@
 <template>
-    <div class="header">
-        <slot name="header"/>
+  <div class="page-content">
+    <div class="landing-card">
+      <slot />
     </div>
-    <div class="page-content">
-        <slot />
-    </div>
-    <div class="footer">
-        <slot name="footer"/>
-    </div>
+  </div>
+  <div class="footer">
+    <slot name="footer" />
+  </div>
 </template>
-
 
 <style lang="scss" scoped>
 .page-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
 
-    display: block;
-    
-    height: 90%;
-    width: 80%;
-    padding: .1vh 0;
-    margin: 0 auto;
+.landing-card {
+  height: 60vh;
+  width: 60vw;
 
-    background-color: $primary-green-color;
+  background-image: radial-gradient(farthest-corner circle at 30% 40%,  $light-purple-color 10%, $primary-purple-color);
+  background-color: $light-purple-color;
 
-    border-style: solid;
-    border-color: $dark-color;
-    border-radius: 0 0 1% 1%;
+
+  border-style: solid;
+  border-radius: 18px;
+  border-width: 2px;
+  border-color: $dark-purple-color;
+
 }
 
 .footer {
-  position: fixed;
-  bottom: 0;
   width: 100%;
-  color: $dark-color;
-  text-align: center;
+  bottom: 0;
   padding: 2vh;
+
+  text-align: center;
+  font-size: 0.5rem;
+
+  color: $gray-color;
 }
 </style>
