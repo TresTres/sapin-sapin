802d41f400116af6febeffac8a9b7c556e14359f
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-03-02 21:24:48 -0500

    cosmetic adjustments

--- frontend/stores/user.ts
+++ frontend/stores/user.ts
@@ -2,7 +2,7 @@ export const getUserStore = defineStore("userStore", {
   state: () => ({
     username: "",
     email: "",
-    dateJoined: JSON.stringify(new Date()),
+    dateJoined: new Date(),
   }),
   persist: {
     storage: persistedState.sessionStorage,
@@ -11,15 +11,15 @@ export const getUserStore = defineStore("userStore", {
     accountAge(state): number {
       const now = new Date();
       const msPerDay = 1000 * 60 * 60 * 24;
-      const dateJoined = Date.parse(state.dateJoined);
-      return Math.abs(now.getTime() - dateJoined) / msPerDay;
+      const dateJoined = new Date(state.dateJoined);
+      return (now.getTime() - dateJoined.getTime()) / msPerDay;
     },
   },
   actions: {
     login(user: UserResponseObject) {
       this.username = user.username;
       this.email = user.email;
-      this.dateJoined = JSON.stringify(new Date(user.date_joined));
+      this.dateJoined = new Date(user.date_joined);
     },
     logout() {
       this.$reset();
