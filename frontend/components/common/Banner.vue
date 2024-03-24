bd67405f0b134c64d6a8692f152f0d91174d7b21
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-03-24 03:47:35 -0400

    initial styles of the banner

--- frontend/components/common/Banner.vue
+++ frontend/components/common/Banner.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="banner-container" v-if="bannerText">
-        <div class="banner-area">
+        <div class="banner-area" :class="bannerType.toLowerCase()">
             <h1 class="logo-font">{{ bannerType }}</h1>
             <p>{{ bannerText }}</p>
         </div>
@@ -18,4 +18,30 @@ defineProps<{
 </script>
 
 <style lang="scss" scoped>
+
+.banner-container {
+    display: flex;
+    justify-content: center;
+    width: 70%;
+    max-height: 20%;
+
+}
+
+.banner-area {
+    padding: 1rem;
+    border-radius: 0.5rem;
+    border: 3px solid;
+
+    h1 {
+        font-size: $medium-text-size;
+    }
+    p {
+        font-size: $standard-text-size;
+    }
+}
+
+
+
+
+
 </style>
