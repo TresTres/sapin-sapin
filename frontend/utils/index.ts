d44bea30922be2a13af87b0701f0378aec393914
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-03-24 01:31:09 -0400

    add enum for banner types

--- frontend/utils/index.ts
+++ frontend/utils/index.ts
@@ -1,3 +1,2 @@
-export * from "./constants/routes";
-export * from "./interfaces/response-objects";
-export * from "./interfaces/state-objects";
+export * from "./constants";
+export * from "./interfaces";
