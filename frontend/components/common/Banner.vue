53f14b5de57c0422f7ea995da2d58156172ec6a3
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-03-24 01:31:25 -0400

    starting point of banner component

---
+++ frontend/components/common/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="banner-container" v-if="bannerText">
+        <div class="banner-area">
+            <h1 class="logo-font">{{ bannerType }}</h1>
+            <p>{{ bannerText }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { BannerType } from '@/utils/constants';
+
+defineProps<{
+    bannerType: BannerType;
+    bannerText: string;
+}>();
+
+</script>
+
+<style lang="scss" scoped>
+</style>
