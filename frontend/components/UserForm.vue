e79850a18e33d3b27e45326ab2ad662c967e6cbc
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-03-25 02:05:04 -0400

    use the banner in the user form as well

--- frontend/components/UserForm.vue
+++ frontend/components/UserForm.vue
@@ -4,7 +4,7 @@
       <h1>{{ title }}</h1>
       <span class="description" v-text="descriptionValue"></span>
     </div>
-    <span class="banner" v-text="bannerText"></span>
+    <CommonBanner v-bind="{ bannerType: BannerType.ERROR, bannerText }" />
     <fieldset class="input-group">
       <div class="control-groups">
         <slot />
