0a8721fd6e277774e4f1a318ffe38b048306442f
Ez A <ezra.ablaza.inbox@gmail.com>
2024-04-20 04:17:47 -0400

    Ezra/workshop activity (#25)
    * some styling for activity form input
    
    * adjust card size for data
    
    * add textarea capability to form
    
    * clean up description input
    
    * show series creation form by default
    
    * standardized font weights and spacings
    
    * adjust activity background height

--- frontend/components/activity/Form.vue
+++ frontend/components/activity/Form.vue
@@ -1,49 +1,66 @@
 <template>
-    <form class="activity-input-form" @submit.prevent="$emit('submit', $event)">
-        <div class="heading">
-            <h2>{{ title }}</h2>
-            <span class="description" v-text="descriptionValue"></span>
+  <form class="activity-input-form" @submit.prevent="$emit('submit', $event)">
+    <div class="heading">
+      <h2>{{ title }}</h2>
+      <span class="description" v-text="descriptionValue"></span>
+    </div>
+    <CommonBanner v-bind="{ bannerType: BannerType.WARNING, bannerText }" />
+    <div class="input-fields-grid">
+      <fieldset class="input-group">
+        <div class="control-groups">
+          <slot />
         </div>
-        <CommonBanner v-bind="{ bannerType: BannerType.WARNING, bannerText }" />
-        <div class="input-fields-grid">
-            <fieldset class="input-group">
-                <div class="control-groups">
-                    <slot />
-                </div>
-            </fieldset>
-        </div>
-        <button type="submit" class="form-submit-button">
-            {{ buttonTitle }}
-        </button>
-    </form>
+      </fieldset>
+    </div>
+    <button type="submit" class="form-submit-button">
+      {{ buttonTitle }}
+    </button>
+  </form>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  title: {
-    type: String,
-    default: "Sign Up",
-  },
-  buttonTitle: {
-    type: String,
-    default: "Sign Up",
-  },
-  descriptionValue: {
-    type: String,
-    default: "",
-  },
-  bannerText: {
-    type: String,
-    default: "",
-  },
-});
-defineEmits(["submit"]);
+  defineProps({
+    title: {
+      type: String,
+      default: "Sign Up",
+    },
+    buttonTitle: {
+      type: String,
+      default: "Sign Up",
+    },
+    descriptionValue: {
+      type: String,
+      default: "",
+    },
+    bannerText: {
+      type: String,
+      default: "",
+    },
+  });
+  defineEmits(["submit"]);
 </script>
 
-<style lange="scss" scoped>
-.input-group {
-  border: none;
-}
+<style lang="scss" scoped>
+  .input-group {
+    border: none;
+  }
 
+  .heading {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 1rem;
 
+ }
+
+ .form-submit-button {
+   padding: 0.5rem 1rem;
+   border: none;
+   border-radius: 0.7em;
+   background-color: $primary-purple-color;
+   color: $white-color;
+   font-size: $medium-large-text-size;
+   cursor: pointer;
+   transition: background-color 0.3s;
+ }
 </style>
