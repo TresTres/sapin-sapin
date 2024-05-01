10d4496907902be1fc7a10c78dfb643743fa776a
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-05-01 01:12:12 -0400

    slight more detail to the series page

--- frontend/pages/data/series-[...slug].vue
+++ frontend/pages/data/series-[...slug].vue
@@ -1,28 +1,32 @@
 <template>
-    <Activity>
-        <template #header>
-            <h2>{{"PROP GOES HERE"}}</h2>
-        </template>
-        <template #content>
-            <div className="graph-area">
-            </div>
+  <Activity>
+    <template #header>
+      <h2>{{ series?.title || "" }}</h2>
+      <p>{{ series?.description || "" }}</p>
+    </template>
+    <template #content>
+      <div className="graph-area"></div>
 
-            <div className="cells-area">
-            </div>
+      <div className="cells-area"></div>
 
-            <div className="edit-area"> 
-            </div>
-
-
-        </template>
-    </Activity>
+      <div className="edit-area"></div>
+    </template>
+  </Activity>
 </template>
 
-<script setup lange="ts">
-  const route = useRoute();
+<script lang="ts" setup>
   definePageMeta({
     title: `User Series`,
     layout: "dashboard",
   });
 
+  const route = useRoute();
+  const dataStore = useDataStore();
+  let series: DataEventSeries | undefined = undefined;
+
+  //retrieve series data
+  onBeforeMount(() => {
+    series = dataStore.getSeries(route.params.slug as string);
+    console.log(series);
+  });
 </script>
