02a531f576152cb08f71e6f69db36983fb7b25d1
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-04-30 20:55:47 -0400

    initial farmework of series page

--- frontend/pages/data/series-[...slug].vue
+++ frontend/pages/data/series-[...slug].vue
@@ -1,8 +1,28 @@
 <template>
-    <h2>Series {{route.params.slug}}</h2>
+    <Activity>
+        <template #header>
+            <h2>{{"PROP GOES HERE"}}</h2>
+        </template>
+        <template #content>
+            <div className="graph-area">
+            </div>
+
+            <div className="cells-area">
+            </div>
+
+            <div className="edit-area"> 
+            </div>
+
+
+        </template>
+    </Activity>
 </template>
 
 <script setup lange="ts">
-    const route = useRoute();
+  const route = useRoute();
+  definePageMeta({
+    title: `User Series`,
+    layout: "dashboard",
+  });
 
 </script>
