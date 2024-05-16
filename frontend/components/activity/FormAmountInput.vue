94afeff4be2a09c5126d75ee1156dcbb97762918
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-05-16 01:36:43 -0400

    handle changes to the amount field

--- frontend/components/activity/FormAmountInput.vue
+++ frontend/components/activity/FormAmountInput.vue
@@ -10,11 +10,12 @@
       <span>{{ unitLabel }}</span> 
       <input
         :id="`${label}-${index}-amount`"
+        ref="amountField"
         class="amount-field"
         :placeholder="placeholder"
         :required="required"
-        v-model="amountValue"
         @change="(event: Event) => handleAmountInput(event)"
+        @focus.stop="setCursorToStart()"
       />
     </div>
   </div>
@@ -24,7 +25,7 @@
   interface Props {
     unitLabel?: string;
     label: string | boolean;
-    index: number;
+    index?: number;
     placeholder: string;
     required?: boolean;
   }
@@ -36,21 +37,42 @@
     required: false,
   });
 
-  const amountValue = defineModel<number>("amountValue", {
+
+  const amountField = ref<HTMLInputElement | null>(null);
+  const amountValue = defineModel<number | string>("amountValue", {
     required: true,
     default: 0,
   });
 
+  
+  // watcher to handle outside model changes
+  watch(amountValue, (newValue) => {
+    if(typeof newValue === "number") {
+      (amountField.value as HTMLInputElement).value = formatValue(newValue.toString());
+    }
+  });
+
+  const setCursorToStart = (): void => {
+    /*
+      This function takes an input event and sets the cursor to the start of the input field.
+      In some browsers, the focus event fires before the cursor is set, so we use setTimeout to ensure the cursor is set.
+    */
+    setTimeout(() => (amountField.value as HTMLInputElement).setSelectionRange(0, 0), .1);
+  }
 
   const handleAmountInput = (event: Event): void => {
+    /*
+      This funciton takes an input event and formats the value to an internationalized float with two decimal places.
+      It then updates the amountValue model with the numerical equivalent.
+    */
     (event.target as HTMLInputElement).value = formatValue((event.target as HTMLInputElement)?.value);
     amountValue.value = convertToFloat((event.target as HTMLInputElement)?.value);
   }
 
   const formatValue = (value: string): string => {
     /*
-        This function takes a string value and converts it to a internationalized
-        float with two decimal places. If the value is not a number, it returns "0.00"
+      This function takes a string value and converts it to a internationalized
+      float with two decimal places. If the value is not a number, it returns "0.00"
     */
     const parsedValue = convertToFloat(value);
     if(isNaN(parsedValue)) return "0.00";
@@ -64,9 +86,9 @@
 
   const convertToFloat = (value: string): number => {
     /*
-        This function takes a string value and converts it to a string with two decimal places.
-        It removes any commas and spaces, and adds .00 to the end if the value is an integer.
-        If the value is not a number, it returns 0.00
+      This function takes a string value and converts it to a numerical string with two decimal places.
+      It removes any commas and spaces, and adds .00 to the end if the value is an integer.
+      If the value is not a number, it returns 0.00
     */
     return parseFloat(value.replace(/(,)?(\s+)?/g, "") + ".00");
   };
